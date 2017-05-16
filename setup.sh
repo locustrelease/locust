9055906052c8ed4403e071afcaa874029b888e53
locustrelease <loustforgae@gmail.com>
2017-05-15 19:44:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-apt-get install -y git
-apt-get install -y python-dev python-pip
+apt-get install -y git python-dev python-pip
 git clone https://github.com/locustio/locust.git
 pip install -e locust
-rm locust -rf
